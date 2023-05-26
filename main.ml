db6884aa5bc163dc9ae50d78c26a652661ef8788
momoladebrouill <momoladebrouill@gmail.com>
2023-05-26 07:45:39 +0200

    esh

--- main.ml
+++ main.ml
@@ -1,14 +1,11 @@
 open Raylib
+open Maths
 
 let w = 1000
 let h = 1000
 let zero = Vector3.create 0. 0. 0.
 
-type r3 = {
-    d : float;
-    theta :float;
-    phi : float;
-}
+type r3 = float*float*float
 
 type status = {
     camera : Camera3D.t;
@@ -21,32 +18,39 @@ let setup () =
   Raylib.set_target_fps 60;
   {
       camera = Camera3D.create zero zero (Vector3.create 0. 1. 0.) 45. CameraProjection.Perspective;
-      pos = {
-          theta = 0.;
-          phi = 0.;
-          d = 10.
-      };
-      cubes = List.init 5 (fun i -> Vector3.create (float_of_int (i/5)) (float_of_int (i mod 5)) 0.)
+      pos = (0.,0.,10.);
+      cubes = List.init 5 (fun i -> Vector3.create 0. 0. (float_of_int i)) 
    }
 
 let r3_to_vec3 pos =
-    Vector3.create (cos pos.theta *. pos.d) ((sin pos.theta +. cos pos.phi) *. pos.d) (sin pos.phi *. pos.d)
+    let x,y,z = pos in
+    Vector3.create x y z
+
+let vec3_to_r3 v = 
+    let open Vector3 in 
+    let a,b,c = x v, y v, z v in (a,b,c)
 
 let ceil_vec3 v = let open Vector3 in create (ceil (x v)) (ceil (y v)) (ceil (z v))
-
-let vec3_to_tuple v = let open Vector3 in x v, y v, z v
 
 let make_them_fall cubes = 
     let is_at x' y' z' cube =
         let open Vector3 in 
         x cube = x' && y cube = y' && z cube = z' 
     in
+    let move t =
+        if (List.exists ((is_at  tx ty (tz -. 1.0)) || ) cubes )|| tz < 1. then  
+            if List.exists (is_at  tx (ty -.1.0) (tz -. 1.0)) cubes then  
+                if List.exists (is_at  (tx -.1.0)  ty (tz -. 1.0)) cubes then Vector3.create tx ty (tz -. 1.0)
+                 else Vector3.create tx ty (tz -. 1.0) 
+            else Vector3.create tx ty (tz -. 1.0) 
+        else t
+
     let rec aux cs acc =
         match cs with
         [] -> acc
-        | t::q -> let tx,ty,tz = vec3_to_tuple t in 
-          aux q ((if (List.exists (is_at (tx -. 1.0) ty tz) cubes || tx < 1.) then t else Vector3.create (tx -. 1.) ty tz )::acc)
-    in aux cubes []
+        | t::q -> let tx,ty,tz = vec3_to_r3 t in 
+          aux q ((move t)::acc)
+    in aux cubes [] |> List.rev
  
 let rec loop s =
      if Raylib.window_should_close () then Raylib.close_window () else 
@@ -57,15 +61,14 @@ let rec loop s =
          end_mode_3d ();
          end_drawing ();
          Camera3D.set_position s.camera (r3_to_vec3 s.pos);
+         Camera3D.set_target s.camera (r3_to_vec3 (s.pos $+ (0.0,0.0,-1.0))); 
          loop {
              camera = s.camera;
-             cubes = if is_key_pressed Key.Space then (ceil_vec3 (r3_to_vec3 s.pos))::s.cubes else make_them_fall s.cubes |> List.rev;
-             pos = {
-                 phi = s.pos.phi +. (if is_key_down Key.Up then 0.01 else if is_key_down Key.Down then -0.01 else 0.);
-                 theta = s.pos.theta +. (if is_key_down Key.Left then 0.01 else if is_key_down Key.Right then -0.01 else 0.);
-                 d = s.pos.d +. (if is_key_down Key.A then 1. else if is_key_down Key.Q then -1. else 0.);
-             }
+             cubes = if is_key_pressed Key.Space then (ceil_vec3 (r3_to_vec3 s.pos))::s.cubes else make_them_fall s.cubes;
+             pos = (let x,y,z = s.pos in let d = 0.5 in
+                  x +. (if is_key_down Key.Right then d else if is_key_down Key.Left then -.d else 0.), 
+                  y +. (if is_key_down Key.Up then d else if is_key_down Key.Down then -.d else 0.), 
+                  z +. (if is_key_down Key.S then d else if is_key_down Key.X then -.d else 0.) )
           }
 
-let () =  
-    setup () |> loop; 
+let () =  setup () |> loop; 
--- maths.ml
+++ maths.ml
@@ -8,10 +8,9 @@ type ray =
  let print_vec v = Printf.printf "%f %f %f\n" v.x v.y v.z
 
  let ($+) a b =
-  { x= a.x+.b.x;
-  y=a.y+.b.y;
-  z=a.z+.b.z}
-
+     let xa,ya,za = a in
+     let xb,yb,zb = b in
+     xa+.xb, ya+.yb, za+.zb
 
  let ($*) m v =
  { x= v.x *. m;
@@ -22,17 +21,17 @@ type ray =
   { x= a.x-.b.x;
   y=a.y-.b.y;
   z=a.z-.b.z}
-
+(*
 let at r t =
     r.origin $+ (t $* r.dir)
-
+*)
  let dot a b =
      a.x*.b.x+.a.y*.b.y+.a.z*.b.z
 
- 
+(* 
  let () =
      let v = {x=1.0;y=1.1;z=1.1} in
      let v' = {x=1.5;y=2.1;z=0.1} in
      print_vec (v $- v');
      print_vec (at {origin = v; dir = v} 10.);
-     print_float ( dot v v')
+     print_float ( dot v v'*)
