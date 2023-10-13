c6bb7d4ab62d733058a470d44a093cabebf75cd3
momoladebrouill <momoladebrouill@gmail.com>
2023-10-13 19:09:56 +0200

    dinguerie

--- Jello/main.ml
+++ Jello/main.ml
@@ -10,13 +10,13 @@ let h = 9*k
 
 (*Constantes*)
 let floor_y = foi h (*sol*)
-let dt = 0.1 
-let mass = 10.0 (*masses des particules de reference*)
+let dt = 1.0/.60.0 
+let mass = 20.0 (*masses des particules de reference*)
 let d_eq = 25.0
 let fac_newt = 0.1
 (*Taille du blob carré*)
-let w_blob = 30
-let h_blob = 30
+let w_blob = 10
+let h_blob = 10
 let n = w_blob * h_blob
 
 type vec = float * float
@@ -24,7 +24,6 @@ type vec = float * float
 type point = {
     pos : vec;
     vit : vec;
-    col : float;(*couleur du point*)
     mass : float;(*couleur du point*)
 }
 
@@ -40,10 +39,9 @@ let zero = 0.0,0.0
 let (+$) (a,b) (c,d) = (a+.c, b+.d)
 let (-$) (a,b) (c,d) = (a-.c, b-.d)
 let ( *$) (a,b) q = (a*.q, b*.q)
-let ps (a,b) (c,d) = a*.b +. c*.d  
+let ps (a,b) (c,d) = a*.c +. b*.d  
 let (+%) = Array.map2 (+$)
 let mult q = Array.map (fun t-> t *$ q)
-let ( *%) l q = Array.map (fun a -> {pos = a.pos *$ q; vit = a.vit *$ q; col = a.col *. q; mass = a.mass}) l
 
 let next  i = (i+1) mod n
 let prev  i = (n+i-1) mod n
@@ -70,53 +68,57 @@ let linked_to i = (*to make a square*)
   let l = (i - w_blob, 1.0)::(i + w_blob, 1.0)::(if i mod w_blob =  0 then east else if i mod w_blob = w_blob-1 then west else east @ west)
   in List.filter (fun (j,_) -> j >= 0 && j < n) l
 
-let bilan_des_forces s c i l dt =
+let bilan_des_forces s i l dt =
     let l = [
         (0.0,9.81) *$ s.mass, Color.green; (*champs de pesanteur*)  
-        s.vit *$ (-10.0/.dt) , Color.blue; (*force de frottement fluide 'on baigne dans de l'huile'*)
-        attrac c s.pos s.mass 100.0, Color.yellow (*force newtonienne avec le centre*)
-  ] @ List.map (fun (i,d) -> f_ressort s l.(i) (d_eq*.d) 300.0, Color.purple) (linked_to i) (*force elastique avec les autres *)
-   @ List.map (fun (i,_) -> f_amortisseur s l.(i) (1.0), Color.raywhite) (linked_to i) (*force damped avec les autres *)
-    in let f = List.fold_left (fun x (f,_) -> x +$ f) zero l in
-    if snd (s.pos +$ (s.vit *$ dt)) > floor_y  then ((0.0, -.2.0*.(abs_f (snd f))), Color.red)::l else l  
-let somme_des_forces i s l c dt = List.fold_left (fun x (f,_) -> x +$ f ) zero (bilan_des_forces s c i l dt) 
+  ] @ List.concat (List.map (fun (i,d) -> 
+      [f_ressort s l.(i) (d_eq*.d) 300.0, Color.purple;(*force elastique avec les autres *)
+      f_amortisseur s l.(i) (-15.0), Color.raywhite](*force damped avec les autres *)
+      ) (linked_to i)) 
+    in if snd (s.pos +$ (s.vit  *$ dt)) > floor_y  then
+         let f = List.fold_left (fun x (f,_) -> x +$ f) zero l in (*RN*)
+          ((0.0, -.2.0*.(abs_f (snd f))), Color.red)::l 
+  else l  
+
+let somme_des_forces i s l dt = List.fold_left (fun x (f,_) -> x +$ f ) zero (bilan_des_forces s i l dt) 
 
 let rec loop status =
   
   if Raylib.window_should_close () then Raylib.close_window () else 
   let t,l,shift = status.t,status.l,status.shift in
+  let time_jumping = is_key_down Key.Space in
+  if ((time_jumping)) || t mod 30 = 0 then begin 
+
   clear_background Color.black;
-  
-  begin_drawing ();
   let px (x,_) = iof (x *. status.z +. (fst shift)) in 
   let py (_,y) = iof (y *. status.z +. (snd shift)) in
-    let ray = 100.0 in
-    let freq = 100.0 in
-    let c = (foi (w/2)) +. ray*.cos ((foi t)/.freq) , (ray*.sin ((foi t)/.freq) -. 100.0 ) in 
-    draw_circle (px c) (py c) 5.0 Color.white;
     let posa = foi (-w), foi h in
     draw_rectangle (px posa) (py posa) ((2*w |> foi)*.status.z |> iof) (500.0*.status.z |> iof)  Color.white; 
     Array.iteri (fun i s ->
-        let f =  (bilan_des_forces s c i l dt) in
+        let f =  (bilan_des_forces s i l dt) in
         let pos = s.pos in
-        (*dessin des masses*)
-          (*draw_circle (px pos) (py pos) 5.0 (Raylib.color_from_hsv 200.0 1.0 1.0) ;*)
-          (*draw_text (string_of_float s.col) (iof x) (iof y) 20 Color.raywhite;*)
         
         (*dessin des forces*)
+        if is_key_down Key.F then 
         List.iter (fun (f,col) -> 
             let end_force = pos +$ (f *$ (fac_newt*.status.z)) in 
-            draw_line (px pos) (py pos) (px end_force) (py end_force) col) f;
+            draw_line (px pos) (py pos) (px end_force) (py end_force) col) f
         (*dessin des ressorts*)  
-        (*List.iter (fun (posb,fac) -> let d = dist pos l.(posb).pos  in
+        else
+        List.iter (fun (posb,fac) -> let d = dist pos l.(posb).pos  in
         draw_line (px pos) (py pos) (px l.(posb).pos) (py l.(posb).pos) (Raylib.color_from_hsv 0.0 (abs_f (d -. d_eq*.fac)/.50.0) 1.0 )) (linked_to i)
-*)
     ) l;
 
-    draw_text (string_of_float status.z) 0 0 20 Color.raywhite;
+  draw_text (string_of_float status.z ^
+"+/- pour le zoom
+F pour le mode forces
+Arrows pour le deplacement
+Space pour le saut temporel"
+    ) 0 0 20 Color.raywhite;
+  begin_drawing ();
   end_drawing ();
-
-  let vitesse = -50.0 in
+  end;
+  let vitesse = -1.0 in
   let shift' = List.fold_left (+$) shift
       [
           if is_key_down Key.Up then 0.0,-.vitesse else zero; 
@@ -125,25 +127,25 @@ let rec loop status =
           if is_key_down Key.Right then vitesse,0.0 else zero; 
      ] 
   in
-  let h = 0.1 in
+  let h = 0.001 in
   let h2 = h/.2.0 in
   let h4 = h*.h/.4.0 in
   let h6 = h/.6.0 in
   let y' = Array.map (fun x -> x.vit) l in
   let y = Array.map (fun x -> x.pos) l in
-  let f y y' dt = Array.init n (fun i -> ((somme_des_forces i {pos = y.(i);vit = y'.(i); col = 10.0; mass = l.(i).mass} l c dt) *$ (1.0/.mass))) in
-  let k1 = f y y'  0.1 in
+  let f y y' dt = Array.init n (fun i -> ((somme_des_forces i {pos = y.(i);vit = y'.(i); mass = l.(i).mass} l dt) *$ (1.0/.mass))) in
+  let k1 = f y y'  0.01 in
   let k2 = f (y +% mult h2 y' ) (y' +% mult h2 k1) h2 in
   let k3 = f (y +% mult h2 y' +% mult h4 k1) (y' +% mult h2 k2) h2 in
   let k4 = f (y +% mult h y' +% mult (h*.h2) k2) (y' +% mult h k3) h in
   let ny = y +% mult h y' +% mult (h*.h6) (k1 +% k2 +% k3) in
   let ny' = y' +% mult h6 (k1 +% mult 2.0 k2 +% mult 2.0 k3 +% k4) in
-  let l' = Array.mapi (fun i x -> { pos = ny.(i); vit = ny'.(i); col = x.col; mass = x.mass}) l in
+  let l' = Array.mapi (fun i x -> { pos = ny.(i); vit = ny'.(i); mass = x.mass}) l in
   loop {
       t = t+1;
       l = l';
       shift = shift';
-      z = max 0.0 ( status.z +. (if is_key_down Key.Kp_add then 0.01 else 0.0) +. (if is_key_down Key.Kp_subtract then -0.01 else 0.0)) 
+      z = let vitesse = 0.01 in max 0.0 ( status.z +. (if is_key_down Key.Kp_add then vitesse else 0.0) +. (if is_key_down Key.Kp_subtract then -.vitesse  else 0.0)) 
    }
 
 let setup () =
@@ -157,9 +159,8 @@ let setup () =
           {
               pos = foi (w/2 + (iof (d_eq*.d_init_fac)) * (i mod w_blob - w_blob/2 )),
                     (foi (h/2)) +. d_eq*.d_init_fac *. (foi (i/h_blob - h_blob/2));
-              vit = zero;
-              col = Random.float 50.;
-              mass = mass   
+              vit = (50.0,0.0);
+              mass = mass (*.(foi i)/.(foi n) *)
           });
       shift = zero;
       z = 1.0;
